f292d05a35fb653465738a4f815fcdbfe75c11cd
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2021-01-18 16:47:34 -0600

    plugin-plugin 14.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `groovy`
-    id("nebula.plugin-plugin") version "14.5.0"
+    id("nebula.plugin-plugin") version "14.6.0"
 }
 
 description = "Template for NetflixOSS projects"
