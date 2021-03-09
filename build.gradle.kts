4d1946cdff358f47338e97efe026ea245b23f6d3
Martin Chalupa <mchalupa@netflix.com>
2021-03-09 12:45:21 -0800

    Update plugin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `groovy`
-    id("nebula.plugin-plugin") version "15.2.0"
+    id("nebula.plugin-plugin") version "15.2.1"
 }
 
 description = "Template for NetflixOSS projects"
